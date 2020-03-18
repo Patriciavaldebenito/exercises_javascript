e378c5c072af6279970c1d2945d2f358233f08e4
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-18 00:49:27 -0300

    se resuelve ejercicios difference diagonal hackerrank, se calcula la diferencia absoluta de una matriz cuadrada donde n-filas y m-columnas son iguales, es decir m=n=3

--- diagonal_difference.js
+++ diagonal_difference.js
@@ -1,1 +1,39 @@
-// en desarrollo
+
+const first_value = (arr) => {
+    let pi = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let value_one = arr[i][pi];
+        pi = pi + 1;
+        sum = sum + value_one;
+
+    }
+    return sum;
+}
+const second_value = (arr) => {
+    let sum = 0;
+    let pi = arr.length - 1;
+    for (let i = 0; i < arr.length; i++) {
+        let value_second = arr[i][pi];
+        pi = pi - 1;
+        sum = sum + value_second;
+    }
+    return sum;
+}
+function diagonalDifference(arr) {
+
+    let x = first_value(arr);
+    let y = second_value(arr);
+
+    return Math.abs(x - y)
+
+}
+
+
+const result = diagonalDifference([
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]);
+
+console.log(result);
